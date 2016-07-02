21e4e50a121fd9bda26fe6dc0f564d9e272af979
Spacekins <spacekins@zoho.com>
2016-07-02 11:03:57 -1000

    added initial server

--- client.js
+++ client.js
@@ -10,50 +10,88 @@
       explains how to use your client, including all available options
 
  */
-var STANDARD=require('./standard');
-var help="help/usage";
-help+="\n command requires a single argument(host/uri)";
-help+="\n example www.devleague.com/apply";
+var STANDARD = require('./standard');
+var help = "help/usage";
+help += "\n command requires a single argument(host/uri)";
+help += "\n example www.devleague.com/apply";
 
-var RequestData=require('./RequestData');
-var requestData= new RequestData();
+var RequestData = require('./RequestData');
+var requestData = new RequestData();
 
-var net=require('net');
+var net = require('net');
 //var standard=require('standard');
 
 var host;
 var uri;
 
-var args=process.argv;
+var responseHeaders;
+var statusLine;
+
+var args = process.argv;
 //console.log(args);
 
-var thisPort=80;
-var thisHost="www.google.com";
-var requestHeader=requestData.RequestGetHeader(thisHost,"HEAD");
+var thisPort = 6969; //80
+var thisHost = "localhost";  //"www.google.com";
+var requestHeader = requestData.RequestGetHeader(thisHost, "HEAD");
 console.log(requestHeader);
 
+
+var initialQuestions = {
+    "Method Type": "",
+    "Display Headers?(y/n)": "",
+    "Which Port To use?": ""
+};
+var lastQuestionKey = "";
 //thisHost="www.devleague.com";
 
-var client=net.createConnection({port:thisPort,host:thisHost},function(responseData){
-  console.log('Connected');
+
+var stdInProcess = process.stdin;
+stdInProcess.setEncoding('utf-8');
+
+var stdOutProcess = process.stdout;
+stdOutProcess.setEncoding('utf-8');
+
+
+
+
+
+var client = net.createConnection({
+    port: thisPort,
+    host: thisHost
+}, function(responseData) {
+    startSession();
+    sendRequest();
 })
 
-client.write(requestHeader,function(){
-  console.log('wrote');
-})
 
+function sendRequest() {
+    client.write(requestHeader, function() {
+        console.log('wrote');
+    })
+}
+
+/*
+client.write(requestHeader, function() {
+    console.log('wrote');
+})
+*/
 /*
 client.on('connect',function(data){
   console.log('in Connect' + data);
 })
 */
 
-client.on('data',function(responseData){
-  console.log('****** Data ******** \n\n' +responseData.toString());
+client.on('data', function(responseData) {
+    console.log('****** Data ******** \n\n' + responseData.toString());
+
+
+    responseHash = setResponseHash(responseData.toString());
+
+    //console.log(responseHash);
 });
 
-client.on('error',function(err){
-  console.log(err);
+client.on('error', function(err) {
+    console.log(err);
 });
 
 
@@ -64,30 +102,83 @@ client.on('error',function(err){
  */
 verifySetArguments();
 
-function verifySetArguments(){
-  if(args.length!=3){
-    console.log(help);
-  }
-  else
-  {
-    return setArguments(args)
-  }
 
-  return false;
+
+function startSession() {
+    console.log('Connected');
+
+    goThroughQuestions();
+
+    stdInProcess.on('data', function(dataIn) {
+        initialQuestions[lastQuestionKey] = dataIn;
+        console.log(dataIn);
+        goThroughQuestions();
+    });
 }
 
-function setArguments(argsToSet){
-  var argValues=argsToSet[2].split('/');
+function verifySetArguments() {
+    if (args.length != 3) {
+        console.log(help);
+    } else {
+        return setArguments(args)
+    }
 
-  if(argValues.length==2){
-    host=argValues[0];
-    uri=argValues[1];
-    console.log('host:' + host + ' uri:'+ uri);
-    return true;
-  }
-  else
-  {
-    console.log(help);
     return false;
-  }
+}
+
+function setArguments(argsToSet) {
+    var argValues = argsToSet[2].split('/');
+
+    if (argValues.length == 2) {
+        host = argValues[0];
+        uri = argValues[1];
+        console.log('host:' + host + ' uri:' + uri);
+        return true;
+    } else {
+        console.log(help);
+        return false;
+    }
+}
+
+function setResponseHash(hashThis) {
+    var lines = hashThis.split('\n');
+    var returnHash = {};
+
+    lines.forEach(function(line, index) {
+        if (index == 0) {
+            statusLine = line;
+        } else {
+
+
+            var lineArray = line.split(':');
+
+            if (lineArray.length == 2) {
+               // console.log('******* Array **********');
+                //  console.log(lineArray);
+                returnHash[lineArray[0].trim()] = lineArray[1].trim();
+            }
+        }
+    });
+
+
+    return returnHash;
+};
+
+
+
+function goThroughQuestions() {
+    console.log(initialQuestions);
+    var thesekeys = Object.keys(initialQuestions);
+
+    console.log(thesekeys);
+
+    for (var i = 0; i < thesekeys.length; i++) {
+        if (initialQuestions[thesekeys[i]] == "") {
+            lastQuestionKey = thesekeys[i];
+            stdOutProcess.write(thesekeys[i]);
+            return false;
+        }
+    }
+
+    return true;
 }
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 var net=require('net');
+var serverConfig={'port':6969};
 
-var server=net.Server;
+var server=net.createServer(function(socket){
+  console.log("Server Created on Port " + socket.port);
+});
 
-server.on('connection',function(){
-  console.log('test');
+
+
+server.on('connection',function(socket){
+  socket.setEncoding('utf-8');
+  console.log('Connected to socket ' + socket.remotePort);
+});
+
+server.listen(serverConfig.port,function(){
+  console.log('listening on port ' + serverConfig.port);
 })
+
+server.on('error',function(err){
+  console.log(err);
+  
+  this.listen(serverConfig.port + 1, function(){
+    console.log('Listening on port',server.address().port);
+  })
+
+});
